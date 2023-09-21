faa7c27c95fcb99aa43de9621a1bd6e064add25a
antTheCodeNinja <139186306+antTheCodeNinja@users.noreply.github.com>
2023-09-20 17:27:48 -0700

    Update pxt.json, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,27 +3,7 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "hammy bad ending":return img`
-..................
-..................
-......ff..........
-..ff.fc3f.........
-.fc3ffc3ffffff....
-.fc33333333333f...
-..f333333331333f..
-.f33333333331333f.
-f3333333333333333f
-f333f33333333f333f
-f33333ffffff33333f
-fc333fbbddddf3333f
-fc333fbfbdfdf3333f
-fc3333ffffff33333f
-.fc3333333333333f.
-..fc33333333333f..
-...fc333333333f...
-....ffffffffff....
-`;
+
         }
         return null;
     })
